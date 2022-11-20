08f0d5373fa4826d381bbe8a9731522d1bae2679
ag3d <sydneybettafish@gmail.com>
2022-11-20 11:39:16 +1100

    gitlink3

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,6 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl, clickLink }) => {
+export const ProjectCard = ({ title, description, imgUrl, clickLink, gitLink }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
